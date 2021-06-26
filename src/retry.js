70b0e545c5f9dae9476191e20451b243acda7b17
Leo Huynh <huynhtuananh21895@gmail.com>
2021-06-26 23:55:20 +0700

    Update main func

--- src/retry.js
+++ src/retry.js
@@ -4,32 +4,22 @@ module.exports = async function retry(asyncJob, maxRetries = 10, delay = 1000, s
 	return new Promise(async (resolve) => {
 		await wait(startAfter)
 
-		let retryCount = 1;
+		let retryCount = 1
 		async function fn() {
 			try {
-				console.log(`[Tiny retry]: ${retryCount} time(s)`);
-				const data = await asyncJob(); // Job will throw error if failed
-
-				// Stop if job done
-				resolve({
-					success: true,
-					message: `Job done after ${retryCount} retries`,
-					data
-				});
+				const data = await asyncJob()
+				resolve({ success: true, data })
 			} catch (err) {
 				if (retryCount >= maxRetries) {
-					resolve({
-						success: false,
-						message: `Retry ${maxRetries} times. Job failed!`
-					});
+					resolve({ success: false })
 				} else {
-					retryCount += 1;
-					await wait(delay);
-					fn();
+					retryCount += 1
+					await wait(delay)
+					fn()
 				}
 			}
 		}
 
-		await fn();
-	});
+		await fn()
+	})
 }
