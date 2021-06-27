60595c5701baf00de3059ff9c7fd48d44a5aa808
Leo Huynh <huynhtuananh21895@gmail.com>
2021-06-27 15:35:31 +0700

    Update docs

--- src/retry.js
+++ src/retry.js
@@ -2,19 +2,20 @@ const wait = require('./wait')
 
 /**
  * @typedef Result
- * @property {Boolean}	success			True/false - whether the job done or not
- * @property {Object}		[data]			The return data from async job if the job success (If not, there will be no data in the result object)
+ * @property {Boolean}		success					True/false - whether the job done or not
+ * @property {Object}		[data]					The return data from async job if the job success (If not, there will be no data in the result object)
+ * @property {Number}		[retryCount]			Number of retries
  */
 
 /**
  * Retry an async job until the job success or stop after a maximum number of retries
  *
- * @param {Promise}   asyncJob           	Async job to try.
- * @param {Number}   	[maxRetries=10]     Maximum times to retry job.
- * @param {Number}   	[delay=1000] 				Time after between each retries in ms.
- * @param {Number}   	[startAfter=0] 			Time to start the 1st try in ms.
+ * @param {Promise}   		asyncJob           		Async job to try.
+ * @param {Number}   		[maxRetries=10]     	Maximum times to retry job.
+ * @param {Number}   		[delay=1000] 			Time after between each retries in ms.
+ * @param {Number}   		[startAfter=0] 			Time to start the 1st try in ms.
 
- * @return {Promise<Result>} 				A promise that contains job data when fulfilled
+ * @return {Promise<Result>} 						A promise that contains job data when fulfilled
  */
 
 module.exports = async function retry(asyncJob, maxRetries = 10, delay = 1000, startAfter = 0) {
