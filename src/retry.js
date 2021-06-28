8ba1756c731828601a44b6f0b09d6222595f3181
Leo Huynh <huynhtuananh21895@gmail.com>
2021-06-28 14:11:15 +0700

    Update params, add funcs

--- src/retry.js
+++ src/retry.js
@@ -11,27 +11,34 @@ const wait = require('./wait')
  * Retry an async job until the job success or stop after a maximum number of retries
  *
  * @param {Promise}   		asyncJob           		Async job to try.
- * @param {Number}   		[maxRetries=10]     	Maximum times to retry job.
+ * @param {Number}   		[maxTries=10]     	Maximum times to retry job.
  * @param {Number}   		[delay=1000] 			Time after between each retries in ms.
  * @param {Number}   		[startAfter=0] 			Time to start the 1st try in ms.
  *
  * @return {Promise<Result>} 						A promise that contains job data when fulfilled
  */
 
-module.exports = async function retry(asyncJob, maxRetries = 10, delay = 1000, startAfter = 0) {
+module.exports = async function retry(asyncJob, options) {
+	const { maxTries = 10, delay = 1000, startAfter = 0, process, errorHandler, check } = options
+
 	return new Promise(async (resolve) => {
 		await wait(startAfter)
 
-		let retryCount = 1
-		async function fn() {
+		let tries = 0
+		const fn = async () => {
 			try {
+				tries += 1
+				process?.(tries)
+
 				const data = await asyncJob()
-				resolve({ success: true, retryCount, data })
+				if (!check?.(data)) throw new Error("Unexpected data")
+
+				resolve({ success: true, tries, data })
 			} catch (err) {
-				if (retryCount >= maxRetries) {
-					resolve({ success: false, retryCount })
+				errorHandler?.(err)
+				if (tries >= maxTries) {
+					resolve({ success: false, tries })
 				} else {
-					retryCount += 1
 					await wait(delay)
 					await fn()
 				}
