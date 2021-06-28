ad9a04c11e509e8b11f8a8b26ce7e504d8fa08a0
Leo Huynh <huynhtuananh21895@gmail.com>
2021-06-28 14:11:28 +0700

    Add test cases

--- src/test.js
+++ src/test.js
@@ -1,43 +1,54 @@
 const retry = require('../dist/retry')
 const wait = require('./wait')
 
-let count1 = 1;
+let count1 = 0;
 const fakeJobThatDoneAfter5Tries = async () => {
-  await wait(2000);
-  if (count1 >= 5) {
-    console.log('Job 1 done!');
+	await wait(2000);
+	count1 += 1;
+	if (count1 < 3) {
+		throw new Error('Job 1 failed!');
+	} else if (count1 < 5) {
+		return false
+	} else {
+		console.log('Job 1 done!');
 		return "Job 1 data"
-  } else {
-    count1 += 1;
-    throw new Error('Job 1 failed!');
-  }
+	}
 };
 
-let count2 = 1;
+let count2 = 0;
 const fakeJobThatDoneAfter10Tries = async () => {
-  await wait(2000);
-  if (count2 >= 10) {
-    console.log('Job 2 done!');
+	await wait(1000);
+	count2 += 1;
+	if (count2 >= 10) {
+		console.log('Job 2 done!');
 		return "Job 2 data"
-  } else {
-    count2 += 1;
-    throw new Error('Job 2 failed!');
-  }
+	} else {
+		throw new Error('Job 2 failed!');
+	}
 };
 
 (async () => {
 	console.log('Try job 1...')
-	console.time('JOB_1_COST')
-	const result1 = await retry(fakeJobThatDoneAfter5Tries, 8, 1000, 1000)
+	console.time('JOB_1_TIME')
+	const result1 = await retry(fakeJobThatDoneAfter5Tries, {
+		process: tries => console.log(`[TRY]: ${tries} time(s)`),
+		errorHandler: err => console.log(err.toString()),
+		check: Boolean,
+		maxTries: 10, delay: 1000, startAfter: 0
+	})
 	console.log('Job 1 result: ', result1)
-	console.log('Time expect: 1 + 2 * 5 + 1 * (5 - 1) = 15s')
-	console.timeEnd('JOB_1_COST')
+	console.log('Time expect: 0 + 2*5 + 1*(5-1) = 14s')
+	console.timeEnd('JOB_1_TIME')
 
 	console.log('\n****************************')
 	console.log('Try job 2...')
-	console.time('JOB_2_COST')
-	const result2 = await retry(fakeJobThatDoneAfter10Tries, 8, 1000, 1000)
+	console.time('JOB_2_TIME')
+	const result2 = await retry(fakeJobThatDoneAfter10Tries, {
+		process: tries => console.log(`[TRY]: ${tries} time(s)`),
+		errorHandler: err => console.log(err.toString()),
+		maxTries: 3, delay: 2000, startAfter: 1000
+	})
 	console.log('Job 2 result: ', result2)
-	console.log('Time expect: 1 + 2 * 8 + 1 * (8 - 1) = 24s')
-	console.timeEnd('JOB_2_COST')
+	console.log('Time expect: 1 + 1*3 + 2*(3-1) = 8s')
+	console.timeEnd('JOB_2_TIME')
 })();
