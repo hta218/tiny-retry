5f675308d379cc433c618f50a18d4653cc2c71e5
Tuan Anh (Leo) <huynhtuananh21895@gmail.com>
2021-07-02 22:34:18 +0700

    Update docs

--- src/retry.js
+++ src/retry.js
@@ -4,20 +4,20 @@ const wait = require('./wait')
  * @typedef Result
  * @property {Boolean}		success					True/false - whether the job done or not
  * @property {Object}		[data]					The return data from async job if the job success (If not, there will be no data in the result object)
- * @property {Number}		[tries]			Number of tries
+* @property {Number}		[tries]					Number of tries
  */
 
 /**
  * Retry an async job until the job success or stop after a maximum number of tries
  *
- * @param {Promise}   		asyncJob           		Async job to try.
- * @param {Object}   		options           		Options
- * @param {Number}   		[options.maxTries=10]     	Maximum times to try job.
+ * @param {Promise}   		asyncJob           			Async job to try.
+ * @param {Object}   		options           			Options
+ * @param {Number}   		[options.maxTries=10]     		Maximum times to try job.
  * @param {Number}   		[options.delay=1000] 			Time after between each try in ms.
  * @param {Number}   		[options.startAfter=0] 			Time to start the 1st try in ms.
  * @param {Function}   		[options.process=void] 			A process function to run before each try with the "tries" count argument
- * @param {Function}   		[options.errorHandler=void] 			A function to handle error in each try with the "err" argument
- * @param {Function}   		[options.check=void => Boolean] 			A function with the job reponse argument to verify whether the job response is expected or not (throw an error if not)
+ * @param {Function}   		[options.errorHandler=void] 		A function to handle error in each try with the "err" argument
+ * @param {Function}   		[options.check=void => Boolean] 	A function with the job reponse argument to verify whether the job response is expected or not (throw an error if not)
  *
  * @return {Promise<Result>} 						A promise that contains job data when fulfilled
  */
